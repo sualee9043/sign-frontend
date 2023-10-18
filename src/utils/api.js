1dba9f616296d919b6e4edfb33742443de501087
otterlee9043 <otterlee99@gmail.com>
2023-10-18 12:18:39 +0900

    fix: 액세스 토큰 만료시 서버에서 재발급한 액세스 토큰 저장하도록 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,25 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+    withCredentials: true
 });
 
+axiosInstance.interceptors.response.use(
+    function (response) {
+        if (response.headers.getAuthorization()) {
+            authApiInstance.defaults.headers.common["Authorization"] = response.headers.getAuthorization();
+        }
+        return response;
+    },
+    async function (error) {
+        return error;
+    }
+);
+
 export const apiInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL,
+    withCredentials: true
 });
 
 export const authApiInstance = axios.create({
@@ -14,7 +28,12 @@ export const authApiInstance = axios.create({
 });
 
 authApiInstance.interceptors.response.use(
-    function (response) {return response;},
+    function (response) {
+        if (response.headers.getAuthorization()) {
+            authApiInstance.defaults.headers.common["Authorization"] = response.headers.getAuthorization();
+        }
+        return response;
+    },
     async function (error) {
         if (error.response.status === 401) {
             console.log(401);
